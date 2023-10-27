d88af4a8abb9e0d5ba51af37dff9237c8a0fcf92
Luke <lucian.io@outlook.com>
2023-10-27 04:45:05 -0400

    useState hooked up

---
+++ app/components/custom-popout.tsx
@@ -0,0 +1,63 @@
+"use client"
+import React, {useState} from "react"
+import {Button} from "@nextui-org/button"
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover"
+import {NextUIProvider} from "@nextui-org/system"
+
+export default function CustomPopout() {
+  const [urlPath, setUrlPath] = useState("")
+  const [filePath, setFilePath] = useState("")
+
+  const handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrlPath(event.target.value)
+  }
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files ? event.target.files[0] : null
+    if (selectedFile) {
+      console.log("Selected file:", selectedFile)
+      setFilePath(selectedFile.name)
+    }
+  }
+
+  return (
+    <NextUIProvider>
+      <Popover placement='bottom' showArrow={true} className='bg-zinc-900'>
+        <PopoverTrigger>
+          <Button className='bg-zinc-950 text-white'>+</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className='px-1 py-2 flex flex-col justify-center'>
+            <div className='text-small font-bold'>Open from URL</div>
+            <div className='text-tiny'>
+              Connect to an existing video on the internet
+            </div>
+            <input
+              className='text-tiny bg-zinc-800 p-1 m-1 rounded-md'
+              value={urlPath}
+              onChange={handleUrlChange}
+            />
+            <div className='text-small font-bold'>Open local file</div>
+            <div className='text-tiny'>Upload a video from the device</div>
+            <div className='text-right p-2 '>
+              <label
+                htmlFor='fileInput'
+                className='text-tiny bg-zinc-800 p-1 px-2 m-1 rounded-md'
+              >
+                Choose File
+              </label>
+              <input
+                type='file'
+                id='fileInput'
+                style={{display: "none"}}
+                onChange={handleFileUpload}
+              />
+            </div>
+            <Button className='bg-zinc-800 text-white'>Add Screen</Button>
+            {filePath && <p>Selected file: {filePath}</p>}
+          </div>
+        </PopoverContent>
+      </Popover>
+    </NextUIProvider>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,14 @@
-"use client"
-import {NextUIProvider} from "@nextui-org/system"
-import {Button} from "@nextui-org/button"
-import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover"
-import {Input} from "@nextui-org/input"
+import CustomPopout from "./components/custom-popout"
 
 export default function Home() {
   return (
-    <NextUIProvider>
-      <div className='flex flex-col h-screen'>
-        <div className='flex-grow flex items-center justify-center '>
-          Splitscreen-Video
-        </div>
-        <div className='fixed bottom-4 left-1/2 transform -translate-x-1/2 '>
-          <Popover placement='bottom' showArrow={true} className='bg-zinc-900'>
-            <PopoverTrigger>
-              <Button className='bg-zinc-950 text-white'>+</Button>
-            </PopoverTrigger>
-            <PopoverContent>
-              <div className='px-1 py-2 flex flex-col justify-center'>
-                <div className='text-small font-bold'>Open from URL</div>
-                <div className='text-tiny'>
-                  Connect to an existing video on the internet
-                </div>
-                <input className='text-tiny bg-zinc-800 p-1 m-1 rounded-md'></input>
-                <div className='text-small font-bold'>Open local file</div>
-                <div className='text-tiny'>Upload a video from device</div>
-                <div className='text-right'>
-                  <button className='text-tiny bg-zinc-800 p-1 px-2 m-1 rounded-md'>
-                    choose file
-                  </button>
-                </div>
-                <Button className='bg-zinc-800 text-white'>Add Screen</Button>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </div>
+    <div className='flex flex-col h-screen'>
+      <div className='flex-grow flex items-center justify-center '>
+        Splitscreen-Video
       </div>
-    </NextUIProvider>
+      <div className='fixed bottom-4 left-1/2 transform -translate-x-1/2 '>
+        <CustomPopout />
+      </div>
+    </div>
   )
 }
