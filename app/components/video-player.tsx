e04e53fe9da267f14eaa008fc17d47b7e67c2a91
Luke <lucian.io@outlook.com>
2023-10-27 18:12:39 -0400

    links work

--- app/components/video-player.tsx
+++ app/components/video-player.tsx
@@ -11,19 +11,32 @@ export default function VideoPlayer() {
   }
 
   const {urlPath, filePath, setUrlPath, setFilePath} = urlStore
+  const videoId = extractVideoId(urlPath)
 
   return (
-    <iframe
-      style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-      }}
-      src={urlPath}
-      title='YouTube Video'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-    ></iframe>
+    <div>
+      <iframe
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+        src={`https://www.youtube.com/embed/${videoId}`} // Embed the video using the extracted ID
+        title='YouTube Video'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+      ></iframe>
+    </div>
   )
+}
+
+function extractVideoId(url: String) {
+  const regex = /[?&]v=([^&]+)/
+  const match = url.match(regex)
+  if (match) {
+    return match[1]
+  } else {
+    return null
+  }
 }
