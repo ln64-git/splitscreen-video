5a26dece125814f0ad0a418ebfcd68e864b4a045
Luke <lucian.io@outlook.com>
2023-11-11 23:55:34 -0500

    next optimizations

--- components/splash-screen.tsx
+++ components/splash-screen.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 const Title = () => (
   <h1 className=' text-4xl py-4 font-bold'>Splitscreen Video</h1>
 )
@@ -34,7 +36,7 @@ const BindingsChart = () => {
 const SplashScreen = () => (
   <div>
     <div className='flex flex-col items-center justify-center w-full pb-8'>
-      <img src='/Back-to-the-Future.gif' className='max-w-lg' alt='' />
+      <Image src='/Back-to-the-Future.gif' className='max-w-lg' alt='' />
       <p className='text-zinc-300 italic text-4xl max-w-3xl font-light '>
         "Art off. OK, I want channels 18, 24, 63, 109, 87 and the weather
         channel." - <strong>Marty McFly</strong>
