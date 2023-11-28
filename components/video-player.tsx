4867d189185dddb37b7d4aff272c15453d707490
Luke <lucian.io@outlook.com>
2023-11-28 05:07:55 -0500

    added mild video player responsiveness

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,57 +1,57 @@
-"use client"
-import React, {useEffect, useState} from "react"
-import {useVideoStore, Video} from "../utils/video-store"
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useVideoStore, Video } from '../utils/video-store';
 
 export default function VideoPlayer() {
-  const [localVideos, setLocalVideos] = useState<Video[]>([])
-  const videoStore = useVideoStore()
+  const [localVideos, setLocalVideos] = useState<Video[]>([]);
+  const videoStore = useVideoStore();
 
   useEffect(() => {
-    setLocalVideos(videoStore.videos)
-  }, [videoStore.videos])
+    setLocalVideos(videoStore.videos);
+  }, [videoStore.videos]);
 
   function generateIframeSrc(video: Video) {
     if (video && video.isRemote) {
-      return getYoutubeVideo(video.path)
+      return getYoutubeVideo(video.path);
     } else if (video && video.file) {
-      return video.file && URL.createObjectURL(video.file)
+      return video.file && URL.createObjectURL(video.file);
     }
-    return ""
+    return '';
   }
 
   function getYoutubeVideo(url: string | undefined) {
-    const regex = /[?&]v=([^&]+)/
-    const videoId = url?.match(regex)
+    const regex = /[?&]v=([^&]+)/;
+    const videoId = url?.match(regex);
     if (videoId) {
-      return "https://www.youtube.com/embed/" + videoId[1]
+      return 'https://www.youtube.com/embed/' + videoId[1];
     } else {
-      return ""
+      return '';
     }
   }
 
   const splitScreen = (videosArray: Video[]) => {
-    const columns = Math.ceil(Math.sqrt(videosArray.length))
+    const columns = Math.ceil(Math.sqrt(videosArray.length));
     return (
-      <div className='w-full h-full absolute flex flex-wrap'>
+      <div className="absolute flex h-full w-full flex-col flex-wrap 2xl:flex-row">
         {videosArray.map((video: Video, index: number) => (
           <div
             key={index}
-            className='relative flex-1'
-            style={{flexBasis: `calc(${100 / columns}%)`}}
+            className="flex-basis relative flex-1"
+            style={{ flexBasis: `calc(${100 / columns}%)` }}
           >
             <iframe
               src={generateIframeSrc(video)}
               title={`Video Player ${index}`}
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              className='w-full h-full'
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="h-full w-full"
             ></iframe>
           </div>
         ))}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <div className='w-full h-full absolute'>{splitScreen(localVideos)}</div>
-  )
+    <div className="absolute h-full w-full">{splitScreen(localVideos)}</div>
+  );
 }
